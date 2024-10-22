ca56d7fa84247feb2fc28bf40af03a77e7199a63
RahilGhanem <146113919+RahilGhanem@users.noreply.github.com>
2024-10-22 23:54:06 +0100

    Delete subtraction.py

--- subtraction.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify
-
-subtraction_bp = Blueprint('subtraction', name)
-
-@subtraction_bp.route('/minus/<float:A>/<float:B>', methods=['GET'])
-def minus(A, B):
-    result = A - B
-    return jsonify({'status': 200, 'result': result})

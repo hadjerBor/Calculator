2e3801ce0a64b636791beb68366795fd1043e8d5
hadjerBor <146716657+hadjerBor@users.noreply.github.com>
2024-10-26 10:36:23 +0100

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,7 +1,5 @@
 from flask import jsonify
 
-
-@app.route('/add/<int:num1>/<int:num2>', methods=['GET'])
-def add(num1, num2):
-    result = num1 + num2
+def add(numberA, numberB):
+    result = numberA + numberB
     return jsonify({'status': 200, 'result': result})
